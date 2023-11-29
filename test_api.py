1145e01d04950b5a103274268ef893239e486661
Josh <jsimnitt@gmail.com>
2023-11-28 22:34:22 -0700

    test changes for api

--- test_api.py
+++ test_api.py
@@ -1,44 +1,38 @@
-import os
 import unittest
-from unittest.mock import patch
-from api import create_request_url
+from unittest.mock import patch, MagicMock
+import api
 
 
-class TestApi(unittest.TestCase):
+class TestJiraAPIProgram(unittest.TestCase):
 
-    @patch('api.JIRA_SITE_URL', 'http://test-jira.com')
+    @patch('api.load_dotenv')
+    def test_load_env_variables(self, mock_load_dotenv):
+        api.load_dotenv()
+        mock_load_dotenv.assert_called_once()
+
     def test_create_request_url(self):
-        # Test the create_request_url function
-        endpoint = '/test-endpoint'
-        expected_url = 'http://test-jira.com/test-endpoint'
-        result_url = create_request_url(endpoint)
-        self.assertEqual(result_url, expected_url)
+        test_endpoint = "/test"
+        expected_url = f"{api.JIRA_SITE_URL}{test_endpoint}"
+        self.assertEqual(api.create_request_url(test_endpoint), expected_url)
 
-    @patch('requests.get')
-    def test_api_call_success(self, mock_get):
-        # Mock a successful API response
-        mock_response = mock.Mock()
-        mock_response.status_code = 200
-        mock_response.json.return_value = {'data': 'test_data'}
-        mock_get.return_value = mock_response
+    def test_create_request_headers_server(self):
+        expected_headers = {'Authorization': f'Bearer {api.JIRA_API_TOKEN}'}
+        self.assertEqual(api.create_request_headers_server(), expected_headers)
 
-        # Call the API function and assert the response
-        response = api_function()  # Replace with actual API function call
-        self.assertEqual(response, {'data': 'test_data'})
+    @patch('api.safe_requests.get')
+    def test_execute_request_server_success(self, mock_get):
+        mock_get.return_value = MagicMock(status_code=200, json=lambda: {"total": 42})
+        response = api.execute_request_server("http://test.com", {}, {})
+        self.assertIsNotNone(response)
+        self.assertEqual(response, {"total": 42})
 
+    @patch('api.safe_requests.get')
+    def test_execute_request_server_failure(self, mock_get):
+        mock_get.side_effect = api.requests.HTTPError()
+        response = api.execute_request_server("http://test.com", {}, {})
+        self.assertIsNone(response)
 
-class TestApiEnvironmentVariables(unittest.TestCase):
-
-    @patch('os.getenv')
-    def test_jira_api_key_retrieval(self, mock_getenv):
-        # Mock the getenv function to return a test API key
-        mock_getenv.return_value = 'test_api_key'
-
-        # Retrieve the API key using the function in api.py
-        api_key = os.getenv('JIRA_API_KEY')  # Replace with actual function call if different
-
-        # Assert that the retrieved API key matches the mock
-        self.assertEqual(api_key, 'test_api_key')
+    # Add more tests for handle_request_error, get_jql_query_results, etc.
 
 
 if __name__ == '__main__':
